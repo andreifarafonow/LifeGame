7009e08542e7cf4b8c983ae87f1ac5eb687ba41c
Андрей <andreifarafonow@mail.ru>
2020-03-24 16:40:51 +0300

    Добавил файлы конфигурации

--- LifeGame/Program.cs
+++ LifeGame/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using GameCore;
+using Microsoft.Extensions.Configuration;
 
 namespace LifeGame
 {
@@ -7,10 +9,16 @@ namespace LifeGame
     {
         static void Main(string[] args)
         {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                                               .SetBasePath(Directory.GetCurrentDirectory())
+                                               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                               .Build();
+
+
             Console.WindowHeight = Console.LargestWindowHeight;
             Console.WindowWidth = Console.LargestWindowWidth;
 
-            var game = new Game(10, 25);
+            var game = new Game(int.Parse(configuration.GetSection("gameHeight").Value), int.Parse(configuration.GetSection("gameWidth").Value));
 
             game.Start();
 
